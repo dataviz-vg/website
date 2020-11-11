fa78c1fc5471de7f7e0f69d61e39d7af23c8d593
colin <colin@thinkr.fr>
2020-11-11 20:48:03 +0100

    Rscript

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ sudo apt update && sudo apt install -y libcurl4-openssl-dev
 
 git remote set-url origin https://x-access-token:${{ secrets.ACCESS_TOKEN }}@https://github.com/datavizvg/website.git netlify
 
-RScript -e 'install.packages(c("remotes", "blogdown"));remotes::install_github("datavizvg/vegviz")'
+Rscript -e 'install.packages(c("remotes", "blogdown"));remotes::install_github("datavizvg/vegviz")'
 
 Rscript -e 'blogdown::install_hugo()'
 
