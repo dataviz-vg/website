bae4b309307f00b0da3c4867fce7e7010a425a33
colin <colin@thinkr.fr>
2020-11-11 21:14:48 +0100

    new deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 sudo apt update && sudo apt install -y libcurl4-openssl-dev
 
-git remote set-url origin https://x-access-token:${{ secrets.ACCESS_TOKEN }}@https://github.com/datavizvg/website.git netlify
+git remote set-url origin https://colinfay:${{ secrets.ACCESS_TOKEN }}@https://github.com/datavizvg/website.git
+git branch --set-upstream-to=upstream/netlify
+
+echo "options(repos = c(CRAN = 'https://packagemanager.rstudio.com/all/latest'), download.file.method = 'libcurl', Ncpus = 4)" >> /usr/local/lib/R/etc/Rprofile.site
 
 Rscript -e 'install.packages(c("remotes", "blogdown"));remotes::install_github("datavizvg/vegviz")'
 
@@ -13,4 +16,4 @@ git config --local user.name "Colin Fay"
 git add .
 git commit -m "Built" -a
 
-git push --force
+git push --force 
