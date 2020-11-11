9e41e7538b5e78ce31f1f736c365f4e7dd42b659
colin <colin@thinkr.fr>
2020-11-11 22:11:26 +0100

    macos should be quicker

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,8 @@
-sudo apt update && sudo apt install -y libcurl4-openssl-dev
+git remote add origin https://datavizvg:${{ secrets.ACCESS_TOKEN }}@https://github.com/datavizvg/website.git
 
-git remote set-url origin https://datavizvg:${{ secrets.ACCESS_TOKEN }}@https://github.com/datavizvg/website.git
+Rscript -e 'options(repos = c(CRAN = "https://packagemanager.rstudio.com/all/latest"), download.file.method = "libcurl", Ncpus = 4);install.packages(c("remotes", "blogdown"));remotes::install_github("datavizvg/vegviz")'
 
-echo "options(repos = c(CRAN = 'https://packagemanager.rstudio.com/all/latest'), download.file.method = 'libcurl', Ncpus = 4)" >> /usr/local/lib/R/etc/Rprofile.site
-
-Rscript -e 'install.packages(c("remotes", "blogdown"));remotes::install_github("datavizvg/vegviz")'
-
-Rscript -e 'blogdown::install_hugo()'
-
-Rscript -e 'blogdown::build_site()'
+Rscript -e 'blogdown::install_hugo();blogdown::build_site()'
 
 git config --global user.name "contact@colinfay.me"
 git config --global user.email "Colin Fay"
