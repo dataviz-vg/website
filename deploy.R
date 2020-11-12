c8da6b55bed54b1f5178b4c3d22601f4a8a4a65f
colin <colin@thinkr.fr>
2020-11-12 12:28:36 +0100

    better install process

--- deploy.R
+++ deploy.R
@@ -2,11 +2,21 @@ options(repos = c(CRAN = "https://packagemanager.rstudio.com/all/latest"), downl
 install.packages("remotes")
 remotes::install_cran("attachment")
 remotes::install_cran("fs")
+
 remotes::install_github("datavizvg/vegviz")
+remotes::install_github("dill/emoGG")
+
 all_pak <- attachment::att_from_rmds("content/")
+all_pak <- all_pak[-which(all_pak == "vegviz")]
+all_pak <- all_pak[-which(all_pak == "emoGG")]
+are_installed <- installed.packages()
+
 for (i in all_pak){
-  remotes::install_cran(i)
+  if (!i %in% are_installed){
+    remotes::install_cran(i)
+  }
 }
+
 blogdown::install_hugo(extended = TRUE)
 blogdown::build_site(build_rmd = TRUE)
 fs::dir_tree()
