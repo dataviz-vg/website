33cc3c95e2f374e49be9b6014d012ccbee9f52e4
colin <colin@thinkr.fr>
2020-11-12 13:26:02 +0100

    with renv

--- deploy.R
+++ deploy.R
@@ -1,21 +1,9 @@
 options(repos = c(CRAN = "https://packagemanager.rstudio.com/all/latest"), download.file.method = "libcurl", Ncpus = 4)
 install.packages("remotes")
-remotes::install_cran("attachment")
-remotes::install_cran("fs")
 
-remotes::install_github("datavizvg/vegviz")
-remotes::install_github("dill/emoGG")
+remotes::install_cran("renv")
 
-all_pak <- attachment::att_from_rmds("content/")
-all_pak <- all_pak[-which(all_pak == "vegviz")]
-all_pak <- all_pak[-which(all_pak == "emoGG")]
-are_installed <- installed.packages()
-
-for (i in all_pak){
-  if (!i %in% are_installed){
-    remotes::install_cran(i)
-  }
-}
+renv::restore()
 
 blogdown::install_hugo(extended = TRUE)
 blogdown::build_site(build_rmd = TRUE)
